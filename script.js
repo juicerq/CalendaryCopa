c610732b93c24b91506cec13eeffb4cae43c51d5
juicerq <julio.cerqueiira@gmail.com>
2022-11-07 16:22:42 -0300

    add options

--- script.js
+++ script.js
@@ -1,34 +1,4 @@
-const date = Number(document.getElementById('date').value)
-const day = String(document.getElementById('day').value)
-
 const cards = document.querySelector('#cards')
-
-const player1 = document.querySelector('#player1').value
-const hour = document.querySelector('#hour').value
-const player2 = document.querySelector('#player2').value
-
-// function createGame(player1, hour, player2){
-//   return `
-//     <li>
-//       <img src="/assets/icon-${player1}.svg" alt="icon ${player1}">
-//       <strong>${hour}</strong>
-//       <img src="/assets/icon-${player2}.svg" alt="icon ${player2}">
-//     </li>`
-// }
-
-// function createCard(){
-//   return `
-//   <div class="card">
-//         <h2>${date} <span>${day}</span></h2>
-//         <ul>
-//           ${game}
-//         </ul>
-//       </div>`
-// }
-
-// document.querySelector("#cards").innerHTML = `
-// ${createCard('24/11', 'quinta', createGame('brazil', '18:00', 'serbia'))}
-// ${createCard('28/11', 'segunda', createGame('serbia', '02:00', 'brazil'))}`]
 
 function createCard(){
   cards.innerHTML += `
